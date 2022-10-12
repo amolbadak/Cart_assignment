d66771acb8c214d5fd85eb6f066de7077b0a3939
amolbadak <113195920+amolbadak@users.noreply.github.com>
2022-10-12 14:37:52 +0530

    Deleted

--- ConsumerLoan/src/main/java/spring/boot/loan/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package spring.boot.loan.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import spring.boot.loan.entity.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, String> {
-
-}
